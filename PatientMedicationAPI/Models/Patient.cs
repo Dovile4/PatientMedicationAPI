dead33a8991c9fcf33a14a5e885a6cfef3defa0a
Dovile4 <dovile.baranauskaite4@gmail.com>
2024-03-24 12:30:51 +0000

    Add initial project

---
+++ PatientMedicationAPI/Models/Patient.cs
@@ -0,0 +1,11 @@
+﻿namespace PatientMedicationAPI.Models
+{
+    public class Patient
+    {
+        int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateOnly DateOfBirth { get; set; }
+        public string Sex { get; set; }
+    }
+}
---
+++ PatientMedicationAPITests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace PatientMedicationAPITests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
