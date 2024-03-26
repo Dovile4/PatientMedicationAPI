e9148ffb0d848084be201096fdbc6e5cbc1e9a70
Dovile4 <dovile.baranauskaite4@gmail.com>
2024-03-26 22:54:07 +0000

    Add Get API to controller

--- PatientMedicationAPI/Controllers/MedicationRequestController.cs
+++ PatientMedicationAPI/Controllers/MedicationRequestController.cs
@@ -22,7 +22,7 @@ namespace PatientMedicationAPI.Controllers
         [HttpPost]
         [ProducesResponseType(StatusCodes.Status201Created)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public async Task<IActionResult> Post([FromBody] MedicationRequestModel request)
+        public async Task<IActionResult> Post([FromBody] AddMedicationRequestModel request)
         {
             if (!ModelState.IsValid)
             {
@@ -53,6 +53,28 @@ namespace PatientMedicationAPI.Controllers
             }
 
             return new BadRequestObjectResult("Could not add medication request");
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> Get(int id, string status = null, DateOnly? startDate = null, DateOnly? endDate = null)
+        {
+            var results = await _databaseService.GetFilteredMedicationRequests(id, status, startDate, endDate);
+
+            if (results == null)
+            {
+                return new BadRequestObjectResult("An error occured when trying to retrieve medication requests");
+            }
+
+            if (results.Count == 0)
+            {
+                return new NotFoundObjectResult("Could not find any medication requests with filters provided");
+            }
+            
+
+            return new OkObjectResult(results);
         }
     }
 }
